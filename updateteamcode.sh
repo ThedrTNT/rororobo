04a421f1c21032cdb13c25fa6693f09f46a0c81f
Kenny Tang <kennyftang@gmail.com>
2017-01-11 17:38:21 -0600

    "Testing"

---
+++ updateteamcode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -ri ./ ~/Programming/Robotics/teamcode/
+cd ~/Programming/Robotics/teamcode/
+echo "Commit Message: "
+read commit
+git add *
+git commit -m "\""$commit"\""
+git push origin master
